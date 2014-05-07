92ff6449e8b1ca897a5a4d70ca5b50b174bb1d70
Peter Manev <petermanev@gmail.com>
2014-05-07 17:36:44 +0200

    added Scirius

--- build-debian-live.sh
+++ build-debian-live.sh
@@ -24,7 +24,12 @@ mkdir -p config/includes.chroot/etc/init.d/
 mkdir -p config/includes.binary/isolinux/
 mkdir -p config/includes.chroot/var/log/suricata/StatsByDate/
 mkdir -p config/includes.chroot/etc/logrotate.d/
-cd ..
+mkdir -p config/includes.chroot/opt/
+cd config/includes.chroot/opt/ && \
+git clone https://github.com/StamusNetworks/scirius.git 
+
+cd ../../../../
+
 
 # add config and menu colored files
 # logstash
@@ -44,17 +49,6 @@ cp staging/etc/logrotate.d/suricata Stamus-Live-Build/config/includes.chroot/etc
 # add the Stmaus Networs logo for the boot screen
 cp staging/splash.png Stamus-Live-Build/config/includes.binary/isolinux/
 
-# ### pre staging Scirius ###
-# mkdir -p ~/Debian-Live/chroot/var/lib/django/
-# tar -C  ~/Debian-Live/chroot/var/lib/django/ -zxf ~/Debian-Live/staging/scirius/scirius.tar.gz
-# cp ~/Debian-Live/staging/scirius/local_settings.py ~/Debian-Live/chroot/var/lib/django/scirius/scirius/local_settings.py
-# chmod 644 ~/Debian-Live/chroot/var/lib/django/scirius/scirius/local_settings.py
-# cp ~/Debian-Live/staging/scirius/django-init ~/Debian-Live/chroot/etc/init.d/django
-# chmod 755 ~/Debian-Live/chroot/etc/init.d/django
-# mkdir -p ~/Debian-Live/chroot/var/lib/django/scirius/django_tables2/
-# cp -r ~/Debian-Live/staging/scirius/django_tables2/*  ~/Debian-Live/chroot/var/lib/django/scirius/django_tables2/
-# ### pre staging Scirius ###
-
 # add packages to be installed
 echo "
 libpcre3 libpcre3-dbg libpcre3-dev 
@@ -63,11 +57,12 @@ libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0
 make flex bison git git-core subversion libmagic-dev libnuma-dev pkg-config 
 ethtool bwm-ng iptraf htop libjansson-dev libjansson4 libnss3-dev libnspr4-dev 
 libgeoip1 libgeoip-dev apache2 openjdk-7-jdk openjdk-7-jre-headless 
-python-django python-flup rsync python-six python-django-south python-git 
-python-crypto libgmp10 libyaml-0-2 python-markupsafe 
-python-pkg-resources python-yaml ssh sudo tcpdump oinkmaster 
-python-jinja2 python-httplib2 " \
+rsync 
+python-crypto libgmp10 libyaml-0-2  
+python-yaml ssh sudo tcpdump oinkmaster 
+python-pip " \
 >> Stamus-Live-Build/config/package-lists/StamusNetworks.list.chroot
+
 
 # add specific tasks(script file) to be executed 
 # inside the chroot environment
