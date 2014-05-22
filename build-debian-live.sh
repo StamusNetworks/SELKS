0f865d6eaed04c88f79f15d71e3045ad9e42a413
Eric Leblond <eleblond@stamus-networks.com>
2014-05-22 12:14:02 +0200

    scirius: get version 0.3
    As git master can be unstable, it is safer to build with a released
    version.

--- build-debian-live.sh
+++ build-debian-live.sh
@@ -36,7 +36,7 @@ mkdir -p config/includes.chroot/etc/suricata/rules/
 mkdir -p config/includes.chroot/etc/kibana/
 mkdir -p config/includes.chroot/root/Desktop/
 cd config/includes.chroot/opt/selks/ && \
-git clone https://github.com/StamusNetworks/scirius.git 
+git clone -b scirius-0.3 https://github.com/StamusNetworks/scirius.git
 cd ../../../../../
 
 
