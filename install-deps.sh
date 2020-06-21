0e2ac47fd1bc3f18bfe44d6e06acc4d0425aa659
Peter Manev <pmanev@stamus-networks.com>
2020-06-21 16:04:20 +0200

    install: Remove old package requirement

--- install-deps.sh
+++ install-deps.sh
@@ -11,6 +11,6 @@ set -e
 apt-get -y install xorriso live-build syslinux squashfs-tools python-docutils
 
 # Install needed packages for the choose your own kernel option
-apt-get -y install wget fakeroot kernel-package gcc libncurses5-dev bc \
+apt-get -y install wget fakeroot gcc libncurses5-dev bc \
 ca-certificates pkg-config make flex bison build-essential autoconf \
 automake aptitude
