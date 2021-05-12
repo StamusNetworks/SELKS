04ab09c45b4bbf48585bf3d470238d8ff375e426
Raphaël Brogat <rbrogat@stamus-networks.com>
2021-05-12 16:39:21 +0200

    Allow to define a custom path for suricata logs

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -28,6 +28,10 @@
 #      exit 1
 # fi
 
+#############
+# INTERFACE #
+#############
+
 function getInterfaces {
   intfnum=0
   for interface in $(ls /sys/class/net); do echo "${intfnum}: ${interface}"; ((intfnum++)) ; done
@@ -74,13 +78,43 @@ do
   INTERFACES_LIST=${INTERFACES_LIST}\ -i\ ${interface}
 done
 
-echo INTERFACES=$INTERFACES_LIST > .env
+echo "INTERFACES=${INTERFACES_LIST}" > .env
+
+
+##############
+# DEBUG MODE #
+##############
+
 
 while true; do
     read -p "Do you want to use debug mode?[Y/N] " yn
     case $yn in
-        [Yy]* ) echo SCIRIUS_DEBUG=True >> .env; echo NGINX_EXEC=nginx-debug >> .env; break;;
-        [Nn]* ) exit;;
+        [Yy]* ) echo "SCIRIUS_DEBUG=True" >> .env; echo "NGINX_EXEC=nginx-debug" >> .env; break;;
+        [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+######################
+# SURICATA LOGS PATH #
+######################
+BASEDIR=$(dirname "$0")
+echo -e "With SELKS running, packets captures can take up a lot of disk space"
+echo -e "You might want to save them on an other disk/partition"
+echo -e "Current partition free space :$(df --output=avail -h . | tail -n 1 )"
+echo -e "Please give the path where you want the captures to be saved, or hit enter to use the default value."
+echo -e "Default : [${BASEDIR}/containers-data/suricata/log]"
+
+read suricata_logs_path
+
+if ! [ -z "${suricata_logs_path}" ]; then
+
+  if ! [ -w suricata_logs_path ]; then 
+    echo -e "\n\nYou don't seem to own write access to this directory\n"
+    echo -e "Please give the path where you want the captures to be saved, or hit enter to use the default value."
+    echo -e "Default : [${BASEDIR}/containers-data/suricata/log]"
+    read suricata_logs_path
+
+  fi
+echo "SURICATA_LOGS_PATH=${suricata_logs_path}" >> .env
+fi
