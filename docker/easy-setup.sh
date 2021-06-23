20cf6a5104ff9e9a5d85e5f7193eb52c8d0a71ee
Raphaël Brogat <rbrogat@stamus-networks.com>
2021-06-23 13:40:33 +0200

    more options including non interactive mode and help()

--- docker/easy-setup.sh
+++ docker/easy-setup.sh
@@ -19,56 +19,135 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-
-function Version(){
-  # $1-a $2-op $3-$b
-  # Compare a and b as version strings. Rules:
-  # R1: a and b : dot-separated sequence of items. Items are numeric. The last item can optionally end with letters, i.e., 2.5 or 2.5a.
-  # R2: Zeros are automatically inserted to compare the same number of items, i.e., 1.0 < 1.0.1 means 1.0.0 < 1.0.1 => yes.
-  # R3: op can be '=' '==' '!=' '<' '<=' '>' '>=' (lexicographic).
-  # R4: Unrestricted number of digits of any item, i.e., 3.0003 > 3.0000004.
-  # R5: Unrestricted number of items.
-  local a=$1 op=$2 b=$3 al=${1##*.} bl=${3##*.}
-  while [[ $al =~ ^[[:digit:]] ]]; do al=${al:1}; done
-  while [[ $bl =~ ^[[:digit:]] ]]; do bl=${bl:1}; done
-  local ai=${a%$al} bi=${b%$bl}
-
-  local ap=${ai//[[:digit:]]} bp=${bi//[[:digit:]]}
-  ap=${ap//./.0} bp=${bp//./.0}
-
-  local w=1 fmt=$a.$b x IFS=.
-  for x in $fmt; do [ ${#x} -gt $w ] && w=${#x}; done
-  fmt=${*//[^.]}; fmt=${fmt//./%${w}s}
-  printf -v a $fmt $ai$bp; printf -v a "%s-%${w}s" $a $al
-  printf -v b $fmt $bi$ap; printf -v b "%s-%${w}s" $b $bl
-
-  case $op in
-    '<='|'>=' ) [ "$a" ${op:0:1} "$b" ] || [ "$a" = "$b" ] ;;
-    * )         [ "$a" $op "$b" ] ;;
-  esac
+############################################################
+# Help Function       #
+############################################################
+function Help(){
+  # Display Help
+  echo
+  echo "SELKS setup script"
+  echo
+  echo "    Syntax: easy-setup.sh [-h|--help] [-d|--debug] [-i|--interfaces <eth0 eth1 eth2 ...>] [-n|--non-interactive] [--skip-checks] [--scirius-version <version>] [--elk-version <version>] [--es-datapath <path>]"
+  echo
+  echo "OPTIONS"
+  echo "   -h, --help"
+  echo "        Display this help menu"
+  echo "   -d,--debug"
+  echo "        Activate debug mode for scirius and nginx."
+  echo "        The interactive prompt regarding this option will be skipped"
+  echo "   -i,--interface <interface>"
+  echo "        Defines an interface on which SELKS should listen."
+  echo "        This options can be called multiple times. Ex : easy-setup.sh -i eth0 -i eth1"
+  echo "        The interactive prompt regarding this option will be skipped"
+  echo "   -n,--non-interactive"
+  echo "        Run the script without interacive prompt. This will activate the '--skip-checks' option. '--interfaces' option is required"
+  echo "   --skip-checks"
+  echo "        Run the scirpt without checking if docker and docker-compose are installed"
+  echo "   --scirius-version <version>"
+  echo "        Defines the version of scirius to use. The version can be a branch name, a github tag or a commit hash. Default is 'master'"
+  echo "   --elk-version <version>"
+  echo "        Defines the version of the ELK stack to use. Default is '7.12.0'. The version should match a tag of Elasticsearch, Kibana and Logstash images on the dockerhub"
+  echo "   --es-datapath <path>"
+  echo "        Defines the path where Elasticsearch will store it's data. The path must already exists and the current user must have write permissions. Default will be in a named docker volume ('/var/lib/docker')"
+  echo "        The interactive prompt regarding this option will be skipped"
+  echo "   --print-options"
+  echo "        Print how the command line options have been interpreted "
 }
 
 
+# Parse command-line options
+
+# Option strings
+SHORT=hdi:n
+LONG=help,debug,interfaces:,non-interactive,skip-checks,scirius-version:,elk-version:,es-datapath:,print-options
+
+# read the options
+OPTS=$(getopt -o $SHORT -l $LONG --name "$0" -- "$@")
+
+if [ $? != 0 ] ; then echo "Failed to parse options...exiting." >&2 ; exit 1 ; fi
+
+eval set -- "$OPTS"
+
+# set initial values
+INTERACTIVE="true"
+DEBUG="false"
+SKIP_CHECKS="false"
+INTERFACES=""
+ELASTIC_DATAPATH=""
+PRINT_PARAM="false"
+# extract options and their arguments into variables.
+while true ; do
+  case "$1" in
+    -h | --help )
+      Help
+      exit
+      ;;
+    --print-options )
+      PRINT_PARAM="true"
+      shift
+      ;;
+    -d | --debug )
+      DEBUG="true"
+      shift
+      ;;
+    -i | --interfaces )
+      INTERFACES="${INTERFACES} $2"
+      shift 2
+      ;;
+    -n | --non-interactive )
+      INTERACTIVE="false"
+      SKIP_CHECKS="true"
+      shift
+      ;;
+    --skip-checks )
+      SKIP_CHECKS="true"
+      shift
+      ;;
+    --scirius-version )
+      SCIRIUS_VERSION="$2"
+      shift 2
+      ;;
+    --elk-version)
+      ELK_VERSION="$2"
+      shift 2
+      ;;
+    --es-datapath)
+      ELASTIC_DATAPATH="$2"
+      shift 2
+      ;;
+      
+    -- )
+      shift
+      break
+      ;;
+    *)
+      echo "Internal error!"
+      exit 1
+      ;;
+  esac
+done
+
+if [[ "${INTERACTIVE}" == "false" ]] && [[ "${INTERFACES}" == "" ]]; then
+  echo "ERROR: --non-interactive option must be use with --interface option"
+  exit
+fi
+
+if [[ "${PRINT_PARAM}" == "true" ]]; then
+  # Print the variables
+  echo "DEBUG = ${DEBUG}"
+  echo "INTERFACES = ${INTERFACES}"
+  echo "INTERACTIVE = ${INTERACTIVE}"
+  echo "SKIP_CHECKS = ${SKIP_CHECKS}"
+  echo "SCIRIUS_VERSION = ${SCIRIUS_VERSION}"
+  echo "ELK_VERSION = ${ELK_VERSION}"
+  echo "ELASTIC_DATAPATH = ${ELASTIC_DATAPATH}"
+  read
+fi
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+##########################
+# Check Curl and Time         #
+##########################
 curl=$(curl -V)
 if [[ -z "$curl" ]]; then
   echo -e "\n\n  Please install curl and re-run the script\n"
@@ -83,22 +162,10 @@ else
   clear
 fi
 
-############################################################
-# Process the input options. Add options as needed.        #
-############################################################
-# Get the options
-while getopts ":hv:" option; do
-   case $option in
-      h) # display Help
-         Help
-         exit;;
-      v) # Enter a name
-         SCIRIUS_VERSION=$OPTARG;;
-     \?) # Invalid option
-         echo "Error: Invalid option"
-         exit;;
-   esac
-done
+
+##########################
+# Set the colors         #
+##########################
 
 red=`tput setaf 1``tput bold`
 green=`tput setaf 2``tput bold`
@@ -106,7 +173,7 @@ reset=`tput sgr0`
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 
-echo -e "DISCLAIMER : This conveniance script comes with absolutely no warranty. It provides a quick and easy way to install SELKS on your system\n
+echo -e "DISCLAIMER : This script comes with absolutely no warranty. It provides a quick and easy way to install SELKS on your system\n
 Altough this script should run properly on major linux distribution, it has only been tested on Debian 10 (buster)\n\n
 Press any key to continue or ^c to exit"
 read
@@ -157,80 +224,110 @@ function install_portainer(){
   echo -e "${green}+${reset} Portainer has been installed and will be available on port 9000" || \
   echo -e "${red}-${reset} Portainer installation failed\n"
 }
+function Version(){
+  # $1-a $2-op $3-$b
+  # Compare a and b as version strings. Rules:
+  # R1: a and b : dot-separated sequence of items. Items are numeric. The last item can optionally end with letters, i.e., 2.5 or 2.5a.
+  # R2: Zeros are automatically inserted to compare the same number of items, i.e., 1.0 < 1.0.1 means 1.0.0 < 1.0.1 => yes.
+  # R3: op can be '=' '==' '!=' '<' '<=' '>' '>=' (lexicographic).
+  # R4: Unrestricted number of digits of any item, i.e., 3.0003 > 3.0000004.
+  # R5: Unrestricted number of items.
+  local a=$1 op=$2 b=$3 al=${1##*.} bl=${3##*.}
+  while [[ $al =~ ^[[:digit:]] ]]; do al=${al:1}; done
+  while [[ $bl =~ ^[[:digit:]] ]]; do bl=${bl:1}; done
+  local ai=${a%$al} bi=${b%$bl}
 
+  local ap=${ai//[[:digit:]]} bp=${bi//[[:digit:]]}
+  ap=${ap//./.0} bp=${bp//./.0}
 
-dockerV=$(docker -v)
-if [[ $dockerV == *"Docker version"* ]]; then
-  echo -e "${green}+${reset} Docker installation found: $dockerV"
-else
-  echo -e "${red}-${reset} No docker installation found\n\n  We can try to install docker for you"
-  read -p "  Do you want to install docker automatically? [y/N] " yn
-  case $yn in
-      [Yy]* ) install_docker;;
-      * ) echo -e "  See https://docs.docker.com/engine/install to learn how to install docker on your system"; exit;;
+  local w=1 fmt=$a.$b x IFS=.
+  for x in $fmt; do [ ${#x} -gt $w ] && w=${#x}; done
+  fmt=${*//[^.]}; fmt=${fmt//./%${w}s}
+  printf -v a $fmt $ai$bp; printf -v a "%s-%${w}s" $a $al
+  printf -v b $fmt $bi$ap; printf -v b "%s-%${w}s" $b $bl
+
+  case $op in
+    '<='|'>=' ) [ "$a" ${op:0:1} "$b" ] || [ "$a" = "$b" ] ;;
+    * )         [ "$a" $op "$b" ] ;;
   esac
-fi
-
-dockerV=$(docker version --format '{{.Server.Version}}')
-
-if [[ ! -z "$dockerV" ]]; then
-  echo -e "${green}+${reset} Docker is available to the current user"
-  test_docker_user
-else
-  echo -e "${red}-${reset} Docker engine is not available to the current user.\n  Either allow current user to execute docker commands or run this script as privileged user.\n"
-  read -p "  Do you want to allow '${USER}' to run docker commands? [y/N] " yn
-  case $yn in
-      [Yy]* ) adduser_to_docker; exit;;
-      * ) echo -e "  See https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user to learn how to allow current standard user to run docker commands."; exit;;
-  esac
-fi
-
-dockerV=$(docker version --format '{{.Server.Version}}')
-
-if Version $dockerV '<' 17.06.0; then
-  echo -e "${red}-${reset} Docker version is too old, please upgrade it to 17.06.0 minimum"
-  exit
-fi
-
-#############################
-#      DOCKER-COMPOSE       #
-#############################
-
-dockerV=$(docker-compose --version)
-
-if [[ $dockerV == *"docker-compose version"* ]]; then
-  echo -e "${green}+${reset} docker-compose installation found"
-else
-  echo -e "${red}-${reset} No docker-compose installation found, see https://docs.docker.com/compose/install/ to learn how to install docker-compose on your system"
-  read -p "  Do you want to install docker-compose automatically? [y/N] " yn
-  case $yn in
-      [Yy]* ) install_docker_compose;;
-      * ) echo -e "  See https://docs.docker.com/compose/install/ to learn how to install docker-compose on your system"; exit;;
-  esac
-fi
+}
 
 
-dockerV=( $dockerV )
-dockerV=$( echo ${dockerV[2]} |tr ',' ' ')
-if Version $dockerV '<' 1.27.0; then
-  echo -e "${red}-${reset} Docker version is too old, please upgrade it to 1.27.0 minimum"
-  exit
-fi
-#############################
-#         PORTAINER         #
-#############################
-if $(docker ps | grep -q 'portainer'); then
-  echo -e "  Found existing portainer installation, skipping...\n"
-else
-  echo -e "\n  Portainer is a web interface for managing docker containers. It is recommended if you are not experienced with docker."
-  while true; do
-      read -p "  Do you want to install Portainer ? [y/n] " yn
-      case $yn in
-          [Yy]* ) install_portainer; break;;
-          [Nn]* ) break;;
-          * ) echo -e "  Please answer Y or N";;
-      esac
-  done
+if [[ "${SKIP_CHECKS}" == "false" ]] ; then
+  dockerV=$(docker -v)
+  if [[ $dockerV == *"Docker version"* ]]; then
+    echo -e "${green}+${reset} Docker installation found: $dockerV"
+  else
+    echo -e "${red}-${reset} No docker installation found\n\n  We can try to install docker for you"
+    read -p "  Do you want to install docker automatically? [y/N] " yn
+    case $yn in
+        [Yy]* ) install_docker;;
+        * ) echo -e "  See https://docs.docker.com/engine/install to learn how to install docker on your system"; exit;;
+    esac
+  fi
+
+  dockerV=$(docker version --format '{{.Server.Version}}')
+
+  if [[ ! -z "$dockerV" ]]; then
+    echo -e "${green}+${reset} Docker is available to the current user"
+    test_docker_user
+  else
+    echo -e "${red}-${reset} Docker engine is not available to the current user.\n  Either allow current user to execute docker commands or run this script as privileged user.\n"
+    read -p "  Do you want to allow '${USER}' to run docker commands? [y/N] " yn
+    case $yn in
+        [Yy]* ) adduser_to_docker; exit;;
+        * ) echo -e "  See https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user to learn how to allow current standard user to run docker commands."; exit;;
+    esac
+  fi
+
+  dockerV=$(docker version --format '{{.Server.Version}}')
+
+  if Version $dockerV '<' 17.06.0; then
+    echo -e "${red}-${reset} Docker version is too old, please upgrade it to 17.06.0 minimum"
+    exit
+  fi
+
+  #############################
+  #      DOCKER-COMPOSE       #
+  #############################
+
+  dockerV=$(docker-compose --version)
+
+  if [[ $dockerV == *"docker-compose version"* ]]; then
+    echo -e "${green}+${reset} docker-compose installation found"
+  else
+    echo -e "${red}-${reset} No docker-compose installation found, see https://docs.docker.com/compose/install/ to learn how to install docker-compose on your system"
+    read -p "  Do you want to install docker-compose automatically? [y/N] " yn
+    case $yn in
+        [Yy]* ) install_docker_compose;;
+        * ) echo -e "  See https://docs.docker.com/compose/install/ to learn how to install docker-compose on your system"; exit;;
+    esac
+  fi
+
+
+  dockerV=( $dockerV )
+  dockerV=$( echo ${dockerV[2]} |tr ',' ' ')
+  if Version $dockerV '<' 1.27.0; then
+    echo -e "${red}-${reset} Docker version is too old, please upgrade it to 1.27.0 minimum"
+    exit
+  fi
+  #############################
+  #         PORTAINER         #
+  #############################
+  if $(docker ps | grep -q 'portainer'); then
+    echo -e "  Found existing portainer installation, skipping...\n"
+  else
+    echo -e "\n  Portainer is a web interface for managing docker containers. It is recommended if you are not experienced with docker."
+    while true; do
+        read -p "  Do you want to install Portainer ? [y/n] " yn
+        case $yn in
+            [Yy]* ) install_portainer; break;;
+            [Nn]* ) break;;
+            * ) echo -e "  Please answer Y or N";;
+        esac
+    done
+  fi
+  
 fi
 
 
@@ -283,6 +380,12 @@ echo "#    SETTINGS    #"
 echo "##################"
 echo -e "\n"
 
+
+######################
+# Setting Stack name #
+######################
+echo "COMPOSE_PROJECT_NAME=SELKS" > ${BASEDIR}/.env
+
 #############
 # INTERFACE #
 #############
@@ -298,9 +401,16 @@ function getInterfaces {
   echo -e "OR"
   echo -e "Example: eth1 eth2 eth3"
   echo -e "\nConfigure threat detection for INTERFACE(S): "
-  read interfaces
   
-  echo -e "\nThe supplied network interface(s):  ${interfaces} \n";
+  if [[ "${INTERFACES}" == "" ]] && [[ "${INTERACTIVE}" == "true" ]]; then
+    read interfaces
+  else
+    echo "${INTERFACES}"
+    interfaces=${INTERFACES}
+  fi
+    
+  echo -e "\nThe supplied network interface(s):  ${interfaces}"
+  echo "";
   INTERFACE_EXISTS="YES"
   if [ -z "${interfaces}" ] ; then
     echo -e "\nNo input provided at all."
@@ -335,7 +445,7 @@ do
   INTERFACES_LIST=${INTERFACES_LIST}\ -i\ ${interface}
 done
 
-echo "INTERFACES=${INTERFACES_LIST}" > ${BASEDIR}/.env
+echo "INTERFACES=${INTERFACES_LIST}" >> ${BASEDIR}/.env
 
 
 ##############
@@ -343,16 +453,23 @@ echo "INTERFACES=${INTERFACES_LIST}" > ${BASEDIR}/.env
 ##############
 
 
-while true; do
-    read -p "Do you want to use debug mode?[Y/N] " yn
-    case $yn in
-        [Yy]* ) echo "SCIRIUS_DEBUG=True" >> ${BASEDIR}/.env; echo "NGINX_EXEC=nginx-debug" >> ${BASEDIR}/.env; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+if [[ "${DEBUG}" == "true" ]]; then
+  echo "y"
+  yn="y"
+else
+  if [[ ${INTERACTIVE} == "true" ]]; then
+    read -p "Do you want to use debug mode? [y/N] " yn
+  else
+    echo "n"
+    yn="n"
+  fi
+fi
+case $yn in
+    [Yy]* ) echo "SCIRIUS_DEBUG=True" >> ${BASEDIR}/.env; echo "NGINX_EXEC=nginx-debug" >> ${BASEDIR}/.env; break;;
+    * ) ;;
+esac
 
-echo ""
+echo
 
 ######################
 # ELASTIC DATA PATH #
@@ -367,20 +484,29 @@ echo -e "You might want to save them on an other disk/partition"
 echo -e "Docker partition free space : ${docker_root_dir} - $(df --output=avail -h ${docker_root_dir} | tail -n 1 )"
 echo -e "Please give the path where you want the data to be saved, or hit enter to use a docker volume in the docker path :"
 
-read elastic_data_path
+if [[ "${ELASTIC_DATAPATH}" == "" ]] && [[ "${INTERACTIVE}" == "true" ]]; then
+  read elastic_data_path
+else
+  echo "${ELASTIC_DATAPATH}"
+  elastic_data_path=${ELASTIC_DATAPATH}
+fi
 
 if ! [ -z "${elastic_data_path}" ]; then
 
-  if ! [ -w elastic_data_path ]; then 
+  while ! [ -w "${elastic_data_path}" ]; do 
     echo -e "\nYou don't seem to own write access to this directory\n"
     echo -e "Please give the path where you want the data to be saved, or hit enter to use a docker volume in the docker path :"
-    read elastic_data_path
+    if [[ "${INTERACTIVE}" == "true" ]]; then
+      read elastic_data_path
+    else
+      exit
+    fi
 
-  fi
+  done
 echo "ELASTIC_DATAPATH=${elastic_data_path}" >> ${BASEDIR}/.env
 fi
 
-echo "COMPOSE_PROJECT_NAME=SELKS" >> ${BASEDIR}/.env
+
 
 ######################
 # Generate KEY FOR DJANGO #
@@ -400,10 +526,11 @@ if [ ! -z "${SCIRIUS_VERSION}" ] ; then
 fi
 
 ######################
-# Setting Stack name #
+# Setting ELK VERSION to use #
 ######################
-echo "COMPOSE_PROJECT_NAME=SELKS" >> ${BASEDIR}/.env
-
+if [ ! -z "${ELK_VERSION}" ] ; then
+  echo "ELK_VERSION=$ELK_VERSION" >> ${BASEDIR}/.env
+fi
 
 
 
@@ -416,6 +543,9 @@ echo -e "\n"
 # BUILDING           #
 ######################
 
+echo -e "Pulling containers \n"
+
+docker-compose pull
 
 echo -e "Building containers, this can take a while... (arround 10 minutes)\n"
 
@@ -428,4 +558,4 @@ time docker-compose build >> ${BASEDIR}/build.log
 # Starting           #
 ######################
 echo -e "\n\nTo start SELKS, run 'docker-compose up -d'"
-echo -e "If you have chose to install Portainer, visit http://localhost:9000 to set your portainer password, and select the docker option"
+echo "If you have chose to install Portainer, visit http://localhost:9000 to set your portainer password, and select the docker option"
